6abde78cbc38a24a3999d01f58d880e58aedb3d9
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r1s4.wethinkcode.co.za>
2019-05-30 11:48:42 +0200

    string char first Norme done

--- libc_funcs/ft_strchr.c
+++ libc_funcs/ft_strchr.c
@@ -6,9 +6,11 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 08:55:20 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/05/24 10:51:21 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/05/30 11:48:01 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strchr(const char *str, int c)
 {
@@ -21,5 +23,7 @@ char	*ft_strchr(const char *str, int c)
 			return ((char*)&str[i]);
 		i++;
 	}
+	if (str[i] == c)
+		return ((char*)&str[i]);
 	return (NULL);
 }
