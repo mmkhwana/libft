d42fe50693c1cab96599e5e4c07d962a0abfea9f
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r1s4.wethinkcode.co.za>
2019-06-01 13:32:49 +0200

    mem compare Nonme done

--- libc_funcs/ft_memcmp.c
+++ libc_funcs/ft_memcmp.c
@@ -6,24 +6,24 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 15:01:47 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/05/31 16:24:01 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/01 13:32:06 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memcmp(const void *str1, const void *str2, size_t n)
+int		ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	unsigned char *dst;
-	unsigned char *str;
-	int 			i;
+	unsigned char	*dst;
+	unsigned char	*str;
+	int				i;
 
 	dst = (unsigned char*)str1;
 	str = (unsigned char*)str2;
 	i = 0;
 	if (n == 0)
 		return (0);
-		while (dst[i] == str[i] && n-- > 0)
+	while (dst[i] == str[i] && n-- > 0)
 	{
 		i++;
 		if (n == 0)
@@ -31,7 +31,7 @@ void		*ft_memcmp(const void *str1, const void *str2, size_t n)
 	}
 	if (dst[i] != str[i])
 	{
-		return (dst[i] - str[i]);
+		return ((int)dst[i] - (int)str[i]);
 	}
 	return (0);
 }
