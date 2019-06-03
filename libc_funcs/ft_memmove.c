b5a17d2712af490d2944f3a0838d1c5062a7f174
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r1s4.wethinkcode.co.za>
2019-06-03 08:41:05 +0200

    Mem move Norme done

--- libc_funcs/ft_memmove.c
+++ libc_funcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:25:23 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/05/31 11:45:30 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/03 08:40:16 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void		*ft_memmove(void *dst, const void *str, size_t n)
 	char *str1;
 	char *str2;
 
-	str1 = (char*)str;
-	str2 = (char*)dst;
+	str1 = (char *)str;
+	str2 = (char *)dst;
 	if (str1 < str2)
 		while (n--)
 		{
