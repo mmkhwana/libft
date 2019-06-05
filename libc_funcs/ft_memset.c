636132dde9cf781376c5c67ee0bb51d3408a5d2d
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-05 09:09:07 +0200

    memset Norme done

--- libc_funcs/ft_memset.c
+++ libc_funcs/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 08:46:59 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/03 14:00:28 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/05 09:08:30 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		*ft_memset(void *b, int c, size_t x)
 	str1 = (unsigned char*)b;
 	str2 = (unsigned char)c;
 	i = 0;
-	while (str1[i] != '\0' && x-- != 0)
+	while (x-- != 0)
 	{
 		str1[i] = str2;
 		i++;
