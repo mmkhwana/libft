37e1d4356c13cf1e3f588d00d7c6349bc17ba081
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-05 11:59:52 +0200

    string compare with size Norme done

--- libc_funcs/ft_strncmp.c
+++ libc_funcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:43:43 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/05/31 09:06:33 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/05 11:59:01 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int		ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (n--)
-	{
-		if (str1[i] != str2[i])
-			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && str1[i] != '\0' && i < n - 1)
 		i++;
-	}
-	return (0);
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
