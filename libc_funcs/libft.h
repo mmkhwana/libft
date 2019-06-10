379f5ba6d600b246aebf1b63e8173c7c539ac183
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-10 11:30:30 +0200

    LIBFT

--- libc_funcs/libft.h
+++ libc_funcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 08:32:25 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/07 13:33:30 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/10 11:30:06 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,6 @@ char				*ft_strnew(size_t size);
 void				ft_strdel(char **as);
 void				ft_strclr(char *s);
 void				ft_striter(char *s, void (*f)(char *));
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+char 				*ft_strmap(char const *s, char (*f)(char));
 #endif
