659858de2f3f769b0996368a9ecd8ff1d2271cdb
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-11 14:57:05 +0200

    string sub Norme & Proctected

--- libc_funcs/ft_strsub.c
+++ libc_funcs/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:42:29 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/11 10:59:00 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:56:13 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ char		*ft_strsub(char const *s, unsigned int start, size_t len)
 	char			*str;
 
 	i = 0;
-	str = (char *)malloc(sizeof(char) * len + 1);
-	if (str == NULL)
+	if ((!s) || !(str = (char *)malloc(sizeof(char) * len + 1)))
 		return (NULL);
 	while (i < len)
 	{
