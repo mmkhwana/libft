40868e39167937e58073afaba3dc1ea90df53606
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-11 08:50:36 +0200

    strnstr Norme done

---
+++ libc_funcs/ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 08:19:39 by mmkhwana          #+#    #+#             */
+/*   Updated: 2019/06/11 08:49:24 by mmkhwana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	if (*needle == 0)
+		return ((char*)haystack);
+	while (haystack[i] != 0 && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] != 0 && j + i < len)
+			j++;
+		if (j == ft_strlen(needle))
+			return ((char*)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
