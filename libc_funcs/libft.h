831e7818e47f2937ccaf1f2509eed949cb528152
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-12 10:29:21 +0200

    LIBFT

--- libc_funcs/libft.h
+++ libc_funcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 08:32:25 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/11 17:07:23 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/12 10:28:51 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,11 +53,13 @@ void				ft_strdel(char **as);
 void				ft_strclr(char *s);
 void				ft_striter(char *s, void (*f)(char *));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
-char 				*ft_strmap(char const *s, char (*f)(char));
+char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strequ(char const *s1, char const *s2);
 size_t				ft_strlcat(char *dst, const char *strs, size_t dstsize);
 int					ft_strnequ(char const *str1, char const *str2, size_t n);
-char				*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char				*ft_strnstr(const char *hay, const char *need, size_t len);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
+char				*ft_strjoin(char const *str1, char const *str2);
+
 #endif
