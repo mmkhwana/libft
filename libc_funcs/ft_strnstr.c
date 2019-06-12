90368054e0ddb85ef8b4b52e43fa3b7849444c5e
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-12 10:43:11 +0200

    string n string Nomer done

--- libc_funcs/ft_strnstr.c
+++ libc_funcs/ft_strnstr.c
@@ -6,27 +6,27 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 08:19:39 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/11 08:49:24 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/12 10:42:16 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *hay, const char *need, size_t len)
 {
 	size_t i;
 	size_t j;
 
 	i = 0;
-	if (*needle == 0)
-		return ((char*)haystack);
-	while (haystack[i] != 0 && i < len)
+	if (*need == 0)
+		return ((char*)hay);
+	while (hay[i] != 0 && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && needle[j] != 0 && j + i < len)
+		while (hay[i + j] == need[j] && need[j] != 0 && j + i < len)
 			j++;
-		if (j == ft_strlen(needle))
-			return ((char*)&haystack[i]);
+		if (j == ft_strlen(need))
+			return ((char*)&hay[i]);
 		i++;
 	}
 	return (NULL);
