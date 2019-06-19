8d6f72243bdc06773f772e4db2815baa4e869c38
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-19 07:44:21 +0200

    put number fd

--- libc_funcs/ft_putnbr_fd.c
+++ libc_funcs/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 16:26:49 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/18 17:39:11 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/19 07:42:46 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void		ft_putnbr_fd(int n, int fd)
 {
-		if (n == -2147483648)
-		return (ft_putstr_fd("-2147483648"));
+	if (n == -2147483648)
+		return (ft_putstr_fd("-2147483648", fd));
 	else if (n < 0)
 	{
-		ft_putchar_fd('-');
-		ft_putnbr_fd(n * (-1));
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(n * (-1), fd);
 	}
 	else if (n > 10)
 	{
-		ft_putnbr_fd(n / 10);
-		ft_putchar_fd(n % 10 + '0');
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
 	}
 	else
 	{
-		ft_putchar_fd(n + '0');
+		ft_putchar_fd(n + '0', fd);
 	}
 }
