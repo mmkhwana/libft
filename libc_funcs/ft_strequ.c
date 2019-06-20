af761cebdba9567fdc2ba08578b25f79ad02aca3
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-20 15:32:43 +0200

    string equ Norme done

--- libc_funcs/ft_strequ.c
+++ libc_funcs/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:21:49 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/19 15:21:36 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/20 15:31:42 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,10 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	int		i;
-
-	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	if (*s1 == '\0' || *s2 == '\0')
+	if (ft_strcmp((char *)s1, (char *)s2) == 0)
 		return (1);
-	if (s1 == s2)
-	{
-		while ((s1[i] && s2[i]) && (s1[i] == s2[i]))
-		{
-			i++;
-		}
-		return (1);
-	}
 	else
 		return (0);
 }
