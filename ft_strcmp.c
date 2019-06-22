d6e1af2b14151b8783fe4e890210e72f949a8483
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-22 02:32:11 +0200

    string comp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:55:14 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/05/31 09:05:00 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/22 02:27:04 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,11 @@ int		ft_strcmp(const char *str1, const char *str2)
 	i = 0;
 	while (str1[i] && str2[i] && str1[i] == str2[i])
 		++i;
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	if (((unsigned char)str1[i] - (unsigned char)str2[i]) > 0)
+		return (1);
+	else if (((unsigned char)str1[i] - (unsigned char)str2[i]) < 0)
+		return (-1);
+	else 
+		return (0);
+
 }
