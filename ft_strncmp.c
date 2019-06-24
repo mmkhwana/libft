99580204f807920977fd35306ed955b121016719
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-24 08:31:28 +0200

    *.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:43:43 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/24 08:12:05 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/24 08:24:37 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int		ft_strncmp(const char *str1, const char *str2, size_t n)
 		return (0);
 	while (str1[i] == str2[i] && str1[i] != '\0' && i < n - 1)
 		i++;
-	if (((unsigned char)str1[i] - (unsigned char)str2[i]) > 0)
+	if (((unsigned char)str1[i] - (unsigned char)str2[i]) < 0)
 		return (-1);
-	else if (((unsigned char)str1[i] - (unsigned char)str2[i]) < 0)
+	else if (((unsigned char)str1[i] - (unsigned char)str2[i]) > 0)
 		return (1);
 	else
 		return (0);
