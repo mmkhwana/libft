f85a58e2e21924cdfe9a94fb29afade2fb4a4f4d
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-28 17:26:47 +0200

    *.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 09:44:13 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/05 10:22:18 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/28 17:26:21 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*ft_memcpy(void *dst, const void *str, size_t n)
 	i = 0;
 	if (dst == NULL && str == NULL)
 		return (NULL);
-	while (i < n)
+	while (n-- > 0)
 	{
 		str2[i] = str1[i];
 		i++;
