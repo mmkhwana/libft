148c1eb6f3a8a56b4d6d981d0857eb26c2f4c292
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-29 12:43:42 +0200

    *.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:25:23 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/28 17:43:58 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/29 12:43:17 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@ void		*ft_memmove(void *dst, const void *str, size_t n)
 
 	str1 = (char *)str;
 	str2 = (char *)dst;
-	i = 0;
+	i = n;
 	if (str1 < str2)
-		while (n--)
+		while (i--)
 		{
 			str2[i] = str1[i];
-			i++;
 		}
 	else
 		ft_memcpy(str2, str1, n);
