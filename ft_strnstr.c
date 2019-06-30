bd70b1bd43a5b24e3f9c6daba0c56eed15e0ec98
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-30 13:04:43 +0200

    modified

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,36 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 08:19:39 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/12 10:42:16 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/30 13:03:31 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *hay, const char *need, size_t len)
+char		*ft_strnstr(const char *stack, const char *ned, size_t len)
 {
-	size_t i;
-	size_t j;
+	char	*str;
+	char	*s;
+	size_t	i;
+	int		j;
+	int		c;
 
 	i = 0;
-	if (*need == 0)
-		return ((char*)hay);
-	while (hay[i] != 0 && i < len)
+	c = 0;
+	s = (char *)stack;
+	str = (char *)ned;
+	if (*str == '\0')
+		return (s);
+	while (s[i] != '\0' && i < len)
 	{
-		j = 0;
-		while (hay[i + j] == need[j] && need[j] != 0 && j + i < len)
-			j++;
-		if (j == ft_strlen(need))
-			return ((char*)&hay[i]);
+		j = -1;
+		while (str[++j] != '\0' && (i + j) < len)
+			if (str[j] == s[i + j])
+				c++;
+		if (c == (int)ft_strlen(str))
+			return (s + i);
 		i++;
+		c = 0;
 	}
 	return (NULL);
 }
