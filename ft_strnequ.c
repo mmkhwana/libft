dbbc56829aa07b971b018cdd46d69ffd6bde94ec
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-06-30 12:10:38 +0200

    modified

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 17:43:01 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/11 09:30:28 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/06/30 12:10:02 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 int		ft_strnequ(char const *str1, char const *str2, size_t n)
 {
+	size_t		i;
+
+	i = 0;
 	if (str1 && str2)
 	{
-		while (*str1 == *str2 && n > 0)
+		while (str1[i] == str2[i] && n > 0)
 		{
-			if (*str1 == 0)
+			if (str1[i] == 0)
 			{
 				return (1);
 			}
-			str1++;
-			str2++;
+			i++;
 			n--;
 		}
-		if (n == 0 || (*str1 == 0 && *str2 == 0))
+		if (n == 0 || (str1[i] == 0 && str2[i] == 0))
 		{
 			return (1);
 		}
