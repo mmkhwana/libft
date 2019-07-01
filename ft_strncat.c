fd5c261d77a82c58e66f4b9146f15f9afa057781
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-07-01 08:39:29 +0200

    done

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:43:16 by mmkhwana          #+#    #+#             */
-/*   Updated: 2019/06/30 16:10:44 by mmkhwana         ###   ########.fr       */
+/*   Updated: 2019/07/01 08:39:09 by mmkhwana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ char	*ft_strncat(char *str1, const char *str2, size_t n)
 	size_t		i;
 	size_t		j;
 
-	i = 0;
 	j = 0;
-	while (str1[i] != '\0')
-		i++;
+	i = ft_strlen(str1);
 	while (str2[j] != '\0' && j < n)
 	{
 		str1[i] = str2[j];
